d2e655473bfc8e1ad11d087507bb82136a95b71f
Nikolai Romanov <kolya.rm@gmail.com>
2026-07-04 14:53:51 +0200

    Main part moved to the separate component.

---
+++ frontend/src/components/common/Main.tsx
@@ -0,0 +1,13 @@
+import type { PropsWithChildren } from "react";
+
+function Main({children} : PropsWithChildren) {
+  return(
+    <main className="mx-auto w-full max-w-5xl flex-1 px-8">
+        <div className="grid gap-0 md:grid-cols-[1.7fr_1fr]">
+          {children}
+        </div>
+      </main>
+  );
+}
+
+export default Main;
